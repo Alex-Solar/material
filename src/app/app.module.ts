3855cedc712210dc72c51cea1496fdfcbd18a9e5
Alex Torres <alfratomo.1983@gmail.com>
2021-12-28 23:56:05 -0300

    Generando componentes vista en Dashboard y configuracion de ruteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './components/shared/shared.module';
 
 import { LoginComponent } from './components/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    InicioComponent,
+    NavbarComponent,
+    UsuariosComponent,
+    ReportesComponent
   ],
   imports: [
     BrowserModule,
