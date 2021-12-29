a88c4c48a56b902a7890befffc633698f13ba227
Alex Torres <alfratomo.1983@gmail.com>
2021-12-28 21:42:02 -0300

    Generando logica y validaciones a login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { MatSliderModule } from '@angular/material/slider';
 import { LoginComponent } from './components/login/login.component';
@@ -23,7 +25,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatSliderModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      usuario: ["", Validators.required],
+      password: ["", Validators.required]
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  ingresar() {
+    
+    const usuario = this.form.value.usuario;
+    const password = this.form.value.password;
+
+    console.log(usuario);
+    console.log(password);
   }
 
 }
