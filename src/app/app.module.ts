3c5fb4058e1588160863330181d1e789394d50d8
Alex Torres <alfratomo.1983@gmail.com>
2021-12-28 23:21:59 -0300

    Generando modulo shared refactorizando codigo con imports de material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './components/shared/shared.module';
 
-
-import { MatSliderModule } from '@angular/material/slider';
 import { LoginComponent } from './components/login/login.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -22,11 +17,7 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    ReactiveFormsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +10,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   form: FormGroup;
+  loading = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) {
     this.form = fb.group({
       usuario: ["", Validators.required],
       password: ["", Validators.required]
@@ -25,8 +27,31 @@ export class LoginComponent implements OnInit {
     const usuario = this.form.value.usuario;
     const password = this.form.value.password;
 
-    console.log(usuario);
-    console.log(password);
+    if(usuario == 'atorres' && password == 'admin123'){
+      // ir a dashboard
+      this.fakeLoading ();
+    } else {
+      // Msje Error
+      this.error();
+      this.form.reset ();
+    }
+
+  }
+
+  error() {
+    this._snackBar.open('Usuario o Contraseña ingresados son incorrectos', '', {
+      duration: 4000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+  }
+
+  fakeLoading() {
+    this.loading = true;
+    setTimeout(() => {
+      //Redireccionar a dashboard
+      this.loading = false;
+    }, 1500)
   }
 
 }
